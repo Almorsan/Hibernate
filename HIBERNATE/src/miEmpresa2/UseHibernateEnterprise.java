13f22b0c23315e0f66ed9259d1ceb05b80c4728f
Almorsan <131298499+Almorsan@users.noreply.github.com>
2024-01-16 21:20:51 +0100

    primer commit

---
+++ HIBERNATE/src/miEmpresa2/UseHibernateEnterprise.java
@@ -0,0 +1,10 @@
+package miEmpresa2;
+
+public class UseHibernateEnterprise {
+	
+	public static void main(String[] args) {
+		HibernateEnterprise h=new HibernateEnterprise();
+		h.addProduct(10, "Teclado", 180);
+		}
+
+}
